35f83e63690e5267efdbedfd3a86e697eb8c201f
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-05-12 19:33:34 +0530

    Update Main.py

--- CacheLayerEviction/Main.py
+++ CacheLayerEviction/Main.py
@@ -9,20 +9,20 @@ from MyMap import Mymap
 
 if __name__ == "__main__":
 
-
-    mymap = Mymap(4) # my map takes two argument cahce limit and threshold of chaining
+     # my map takes two argument cahce limit and threshold of chaining in case of hashcode collisions
+    mymap = Mymap(4)
     mymap.put('roshan', 12)
     mymap.put('Jasper', 14)
     mymap.put('Rishi', 15)
-    mymap.put("Tom", 21)
+    mymap.put('Tom', 21)
     mymap.dll.display()
 
-    # evicted Tom once Anna is added
+    # Evicted Tom once Anna is added
     mymap.put('Anna', 19)
 
     mymap.dll.display()
 
-    # Obsere the change in the list order
+    # Observe the change in the list order
     mymap.get('Rishi')
     mymap.dll.display()
     mymap.get('roshan')
